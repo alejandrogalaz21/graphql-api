540df846e2c0bc27c38395ae0c1f880ba14ddcca
Jesus Alejandro Galaz Icedo <jesus.galaz@softtek.com>
2020-10-21 10:50:53 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import express from 'express'
+import graphqlHTTP from 'express-graphql'
+
+const app = express()
+// app.use('/graphql', graphqlHTTP({ graphiql: true }))
+app.listen(5000, () => console.log('server runing !'))
